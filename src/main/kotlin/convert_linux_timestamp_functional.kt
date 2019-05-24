7bad249ed58a302af2a047a57f743bfb242e1995
milcho.penchev <milcho.penchev@lloydsbanking.com>
2019-05-24 10:24:31 +0100

    Updated functional conversion example to use a more generic AppState data class

--- src/main/kotlin/convert_linux_timestamp_functional.kt
+++ src/main/kotlin/convert_linux_timestamp_functional.kt
@@ -4,49 +4,51 @@ import java.io.PrintStream
 import java.text.DateFormat
 import java.util.*
 
+data class AppState(val locale: Locale)
 
 fun main() {
-    Locale.UK.runConvertTimestampProg(Scanner(System.`in`), System.out)
+    AppState(Locale.UK).runConvertTimestampProg(Scanner(System.`in`), System.out)
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-tailrec fun Locale.runConvertTimestampProg(scanner: Scanner, out: PrintStream) {
+tailrec fun AppState.runConvertTimestampProg(scanner: Scanner, out: PrintStream) {
     getUserChoice(scanner, out, this).let { input ->
         when {
             input.startsWith("x", true) -> return
             input.startsWith("l", true) -> readNewLocale(this, scanner, out)
-            else -> input.printDate(out, this).let { this }
+            else -> input.printDate(out, this)
         }
     }.runConvertTimestampProg(scanner, out)
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-fun getUserChoice(scanner: Scanner, out: PrintStream, locale: Locale): String {
-    out.println("Current globalLocale is ${locale.displayName}")
+fun getUserChoice(scanner: Scanner, out: PrintStream, appState: AppState): String {
+    out.println("Current globalLocale is ${appState.locale.displayName}")
     out.println("Enter Linux Timestamp to convert, L to change Locale or X to exit")
     return scanner.nextLine().trim()
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-fun readNewLocale(locale: Locale, scanner: Scanner, out: PrintStream): Locale {
-    out.println("Current globalLocale is ${locale.displayName}")
+fun readNewLocale(appState: AppState, scanner: Scanner, out: PrintStream): AppState {
+    out.println("Current globalLocale is ${appState.locale.displayName}")
     out.println("Select from:\n 1. UK\n 2. US")
     return when (scanner.nextLine().toIntOrNull()) {
-        1 -> Locale.UK
-        2 -> Locale.US
-        else -> println("Invalid locale option entered!").let { locale }
+        1 -> appState.copy(locale = Locale.UK)
+        2 -> appState.copy(locale = Locale.US)
+        else -> println("Invalid locale option entered!").let { appState }
     }
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-fun String.printDate(out: PrintStream, locale: Locale) {
+fun String.printDate(out: PrintStream, appState: AppState): AppState {
     toLongOrNull()?.let { timestamp ->
         DateFormat
-            .getDateTimeInstance(DateFormat.DEFAULT, DateFormat.DEFAULT, locale)
+            .getDateTimeInstance(DateFormat.DEFAULT, DateFormat.DEFAULT, appState.locale)
             .run { out.println("> ${format(Date(timestamp))}\n") }
     } ?: out.println("Invalid timestamp!")
+    return appState
 }
